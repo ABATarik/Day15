e5a243cf180bc63a193e394f9bdff8298ec134be
ABATarik <abaspahic.tarik@outlook.com>
2021-03-23 01:17:00 +0100

    aa,bb,cc,dd

---
+++ ex09/src/index.js
@@ -0,0 +1,16 @@
+const product = (aa, bb, cc, dd ) => {
+
+    const args = [aa, bb, cc, dd];
+    
+ 
+
+    // ne mjenjaj ispod
+    return args.reduce((a,b) => a * b, 1);
+
+};
+console.log(product(1, 2));
+console.log(product(4, 3, 10, 2));
+console.log(product(7));
+console.log(product());
+
+module.exports = product;
